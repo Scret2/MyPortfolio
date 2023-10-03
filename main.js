46ef27d132f62dbe8ff1837731673252a90829d9
Hyogo <106125029+Scret2@users.noreply.github.com>
2023-10-03 12:20:53 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -3,4 +3,19 @@ $(document).ready(() => {
 
   // Add .loaded to .loading
   spinner.classList.add("loaded"); //ロードしたらLoading画面を隠す
+
+  //scroll_effect
+  $(window).scroll(function () {
+    var scrollAnimationElm = document.querySelectorAll(".scroll_up");
+    var scrollAnimationFunc = function () {
+      for (var i = 0; i < scrollAnimationElm.length; i++) {
+        var triggerMargin = 0;
+        if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+          scrollAnimationElm[i].classList.add("on");
+        }
+      }
+    };
+    window.addEventListener("load", scrollAnimationFunc);
+    window.addEventListener("scroll", scrollAnimationFunc);
+  });
 });
