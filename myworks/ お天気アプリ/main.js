c97ad0ed3caa5fe11b5d56e1220818ae75b444c5
Hyogo <106125029+Scret2@users.noreply.github.com>
2023-10-26 12:19:31 +0900

    Add files via upload

--- myworks/ お天気アプリ/main.js
+++ myworks/ お天気アプリ/main.js
@@ -27,7 +27,7 @@ let weatherCodes =
               $("#mino_weather").append(arr[0]["timeSeries"][0]["areas"][0]["weathers"][0], "<br>");
               $("#mino_weather").append(arr[0]["timeSeries"][0]["areas"][0]["winds"][0], "<br>");
               let wcode = arr[0]["timeSeries"][0]["areas"][0]["weatherCodes"][0];
-              //wcode = 400;
+              wcode = 400;
               weatherCodes = $("#mino_weather").append(`<img src="images/` + tr[wcode] + `">`);
               $("#myportfolio").css("color", "#ff8c00");
               if (Math.floor(wcode / 100) == 2) {
@@ -44,6 +44,82 @@ let weatherCodes =
                 //雪
                 $("body").css("background-image", 'url("./images/weather_4.jpg")');
                 $("#myportfolio").css("color", "#666");
+                particlesJS("particles-js", {
+                  particles: {
+                    number: {
+                      value: 100,
+                      density: {
+                        enabled: true,
+                        value_area: 800,
+                      },
+                    },
+                    color: {
+                      value: "fff",
+                    },
+                    shape: {
+                      type: "image",
+                      stroke: {
+                        width: 3,
+                        color: "fff",
+                      },
+                      image: {
+                        src: "./images/snow.png",
+                        width: 120,
+                        height: 120,
+                      },
+                    },
+                    opacity: {
+                      value: 0.7,
+                      random: false,
+                      anim: {
+                        enable: false,
+                        speed: 1,
+                        opacity_min: 0.1,
+                        sync: false,
+                      },
+                    },
+                    size: {
+                      value: 5,
+                      random: true,
+                      anim: {
+                        enable: false,
+                        speed: 20,
+                        opacity_min: 0.1,
+                        sync: false,
+                      },
+                    },
+                    line_linked: {
+                      enable: false,
+                    },
+                    move: {
+                      enable: true,
+                      speed: 3,
+                      direction: "bottom",
+                      random: true,
+                      straight: false,
+                      out_mode: "out",
+                      bounce: false,
+                      attract: {
+                        enable: true,
+                        rotateX: 300,
+                        rotateY: 1200,
+                      },
+                    },
+                  },
+                  interactivity: {
+                    detect_on: "canvas",
+                    events: {
+                      onhover: {
+                        enable: false,
+                      },
+                      onclick: {
+                        enable: false,
+                      },
+                      resize: true,
+                    },
+                  },
+                  retina_detect: true,
+                });
               }
 
               let now = new Date();
